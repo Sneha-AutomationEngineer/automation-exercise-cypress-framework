158b2b410f2e5098cbd4fecdb6287205762e39ad
Sneha Upadhye <sneha.automation.engineer@gmail.com>
2026-07-27 16:27:42 +0530

    fixed import lines

--- cypress/e2e/productsPage.cy.js
+++ cypress/e2e/productsPage.cy.js
@@ -1,4 +1,4 @@
-import HomePage from '../../pages/HomePage';
+import HomePage from '../../pages/homePage';
 import ProductPage from '../../pages/productsPage';
 
 describe('Product Page', function(){
