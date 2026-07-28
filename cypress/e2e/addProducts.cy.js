1bf0d202565e1d79fefab4fc905e863a259671b3
Sneha Upadhye <sneha.automation.engineer@gmail.com>
2026-07-28 23:52:06 +0530

    Implemented add products in cart functionality

---
+++ cypress/e2e/addProducts.cy.js
@@ -0,0 +1,36 @@
+import HomePage from '../../pages/homePage';
+import ProductsPage from '../../pages/productsPage';
+import CartPage from '../../pages/cartPage';
+
+describe('Add Products', function () {
+
+    let homePage;
+    let productsPage;
+    let cartPage;
+    let productsData;
+
+    before(function () {
+        cy.fixture('productsData').then((testData) => {
+            productsData = testData;
+        })
+    })
+
+
+    beforeEach(function () {
+        cy.visit('/');
+        homePage.verifyHomePageDisplayed();
+    })
+    homePage = new HomePage();
+    productsPage = new ProductsPage();
+    cartPage = new CartPage();
+
+    it('Should add products in cart successfully', function () {
+        homePage.openProductsPage();
+        productsPage.captureAndAddFirstTwoProducts(productsData.continueShopping).then((productPrices) => {
+            cartPage.verifyProductsInCart();
+            cartPage.verifyProductPrice(productPrices);
+            cartPage.verifyProductQuantity();
+            cartPage.verifyTotalPriceOfProduct(productPrices);
+        });
+    })
+})
---
+++ pages/cartPage.js
@@ -0,0 +1,34 @@
+export default class CartPage {
+
+    numberOfProducts = 'tbody tr';
+    cartPrice = '.cart_price p';
+    cartQuantity = '.cart_quantity button';
+    cartTotalPrice = '.cart_total_price';
+
+    verifyProductsInCart() {
+        cy.get(this.numberOfProducts).should('have.length', 2);
+    }
+
+    verifyProductPrice(productPrices) {
+        cy.get(this.cartPrice).should('have.length', 2).each(($price, index) => {
+            cy.wrap($price).invoke('text').then((cartPrice) => {
+                expect(cartPrice).to.equal(productPrices[index]);
+            });
+        })
+    }
+
+    verifyProductQuantity() {
+        cy.get(this.cartQuantity).should('have.length', 2).each(($quantity) => {
+            cy.wrap($quantity).should('be.visible').should('have.text', '1');
+        })
+    }
+
+    verifyTotalPriceOfProduct(productPrices) {
+    cy.get(this.cartTotalPrice).should('have.length', 2).each(($totalPrice, index) => {
+            cy.wrap($totalPrice).invoke('text').then((cartPrice) => {
+                expect(cartPrice).to.equal(productPrices[index]);
+            });
+        })
+    }
+
+}
